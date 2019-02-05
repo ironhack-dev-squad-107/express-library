88a1ad6b497ae2d1b0b396cf635a87246bb2e4e3
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2019-02-05 15:07:13 +0100

    Add list of books page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+
+const Book = require("../models/book-model.js");
+
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/books", (req, res, next) => {
+  // whenever a user visits "/books" find all the books sorted by rating
+  Book.find()
+    .sort({ rating: -1 })
+    .then(bookResults => {
+      // send the database query results to the HBS file as "bookArray"
+      res.locals.bookArray = bookResults;
+      res.render("book-list.hbs");
+    })
+    // next(err) skips to the error handler in "bin/www" (error.hbs)
+    .catch(err => next(err));
 });
 
 module.exports = router;
